7903d9e9508e9e1ec7edf38907d6cf51fb4858d8
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2026-06-11 11:55:18 -0400

    d

--- src/components/generalComponents/HomeButton/HomeButton.jsx
+++ src/components/generalComponents/HomeButton/HomeButton.jsx
@@ -5,11 +5,14 @@ import { Link } from "react-router-dom";
 //
 //
 // Component function
-const HomeButton = () => {
+const HomeButton = (props) => {
+  const handleNavBar = () => {
+    props.mobileState.setShowNavBar(false);
+  };
   return (
     // main component container
     <div className={s.home}>
-      <Link className={s.homeLink} to={"/"}>
+      <Link className={s.homeLink} to={"/"} onClick={handleNavBar}>
         <p className={s.buttonText}>Inicio</p>
       </Link>
     </div>
